83ab86470203697ee256b0c3bdffc5dae686c9b7
ANSH GUPTA <anshgupta661@gmail.com>
2020-11-29 23:40:04 +0530

    some more add ons

--- Market-Online-Arzi_App/lib/main.dart
+++ Market-Online-Arzi_App/lib/main.dart
@@ -27,42 +27,36 @@ class _MarketOnlineState extends State<MarketOnline> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          // image: DecorationImage(
-          //   image: AssetImage("images/bk2.jpg"),
-          //   fit: BoxFit.cover,
-          // ),
-          border: Border.all(color: Colors.black87, width: 5.0),
-          borderRadius: BorderRadius.circular(10.0)),
+           image: DecorationImage(
+             image: AssetImage("assests/images/THAPAR_BACKGROUND2.jpg"),
+             fit: BoxFit.cover,
+           ),
+         // border: Border.all(color: Colors.black87, width: 5.0),
+         // borderRadius: BorderRadius.circular(10.0)
+          ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Image.asset('assests/images/thapar_logo.png'),
-          SizedBox(
-            height: 40.0,
+          CircleAvatar(
+            radius: 60.0,
+            backgroundImage: AssetImage('assests/images/thapar_logo2.png'),
           ),
+          // SizedBox(
+          //   height: 40.0,
+          // ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'ARZI\nThapar Institute\nEngineering & Technology',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 38.0,
-                  fontWeight: FontWeight.bold,
-                  color: HexColor("#ff0000"),
-                ),
-              )
-            ],
+            
           ),
           SizedBox(
-            height: 50.0,
+            height: 90.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ButtonTheme(
-                height: 60.0,
+                height: 50.0,
                 minWidth: 180.0,
                 child: RaisedButton(
                   shape: RoundedRectangleBorder(
@@ -86,7 +80,7 @@ class _MarketOnlineState extends State<MarketOnline> {
                 ),
               ),
               ButtonTheme(
-                height: 60.0,
+                height: 50.0,
                 minWidth: 180.0,
                 child: RaisedButton(
                   shape: RoundedRectangleBorder(
