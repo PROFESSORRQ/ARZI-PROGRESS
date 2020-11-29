a64293dccb2256175400bd82cf8fc70849f293d9
shreyshourya <beshrek408>
2020-11-30 00:08:09 +0530

    Leave Form has been added
    PS - Database Connection still not configured

--- Market-Online-Arzi_App/lib/Screens/HomeScreen.dart
+++ Market-Online-Arzi_App/lib/Screens/HomeScreen.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-void main(List<String> args) {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:HomeScreen(),
-  ));
-}
+import 'package:market_online/Screens/LeaveForm.dart';
+
+
+// void main(List<String> args) {
+//   runApp(MaterialApp(
+//     // debugShowCheckedModeBanner: false,
+//     home:HomeScreen(),
+//   ));
+// }
 
 class HomeScreen extends StatefulWidget{
   @override
@@ -14,21 +18,23 @@ class HomeScreen extends StatefulWidget{
 
 class HomeScreenState extends State<HomeScreen> {
 String result="hey there !";
+
 Future _scanQR() async{
   try{
-    String qrResult = await FlutterBarcodeScanner.scanBarcode("#000000","Cancel", true, ScanMode.QR);
+    String qrResult = await FlutterBarcodeScanner.scanBarcode("#000000","Cancel", true, ScanMode.QR)??"Shrey";
     setState(() {
       result=qrResult;
     });
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => LeaveForm()));
   }
   on FormatException{
-setState(() {
-    result="You pressed the back button before scanning";
-     });
+    setState(() {
+      result="You pressed the back button before scanning";
+    });
   }catch (ex){
-setState(() {
+    setState(() {
       result="Unknown Error $ex";
-   });  
+    });
   }
   // }
   // } on FormatException{
@@ -45,16 +51,47 @@ setState(() {
       ),
       body: Center(
         child: Text(
-          result,
+          "Hey there!!",
           style: new TextStyle(fontSize:30.0,fontWeight: FontWeight.bold),
           ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon:Icon(Icons.camera_alt_rounded),
         label: Text("Scan"),
-        onPressed: _scanQR,
+        onPressed: () {
+          _scanQR();
+          Navigator.push(context, MaterialPageRoute(builder: (context) => LeaveForm()));
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+
+// Container(
+// child:Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: <Widget>[
+// Row(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: <Widget>[
+// Text(
+// "Hey there!!",
+// style: new TextStyle(fontSize:30.0,fontWeight: FontWeight.bold),
+// ),
+// ],
+// ),
+// RaisedButton(
+// child: Text(
+// "Scan",
+// textAlign: TextAlign.center,
+//
+// ),
+// onPressed: () {
+// _scanQR();
+// Navigator.push(context, MaterialPageRoute(builder: (context) => LeaveForm()));
+// },
+// ),
+// ],
+// ),
+// ),
